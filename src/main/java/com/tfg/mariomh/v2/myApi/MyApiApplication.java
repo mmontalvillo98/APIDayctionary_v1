65016d217cb7115e924b98ce25b1882ddb91c64e
mmontalvillo98 <133699030+mmontalvillo98@users.noreply.github.com>
2023-11-10 17:29:26 +0100

    API_v1

--- src/main/java/com/tfg/mariomh/v2/myApi/MyApiApplication.java
+++ src/main/java/com/tfg/mariomh/v2/myApi/MyApiApplication.java
@@ -1,10 +1,13 @@
 package com.tfg.mariomh.v2.myApi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoReactiveAutoConfiguration.class})
 @EnableMongoAuditing
 public class MyApiApplication {
 
